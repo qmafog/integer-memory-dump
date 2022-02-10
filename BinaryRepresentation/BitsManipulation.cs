35d322cc6d34f76bd5022f4fbc67a56137f2e50a
Alexander <154324-alexander-rykov@users.noreply.gitlab.com>
2022-02-10 10:42:16 +0300

    Add solution configuration files: editor configs, stylecop settings, style ruleset, readme. Add projects with unit tests.

---
+++ BinaryRepresentation/BitsManipulation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BinaryRepresentation
+{
+    public static class BitsManipulation
+    {
+        /// <summary>
+        /// Get binary memory representation of signed long integer.
+        /// </summary>
+        /// <param name="number">Source number.</param>
+        /// <returns>Binary memory representation of signed long integer.</returns>
+        public static string GetMemoryDumpOf(long number)
+        {
+            throw new NotImplementedException("You need to implement this method.");
+        }
+    }
+}
