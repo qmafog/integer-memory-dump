8ddc08aee26cc369a4edb2f6295f1170c593676a
qmafog <qmafog6@gmail.com>
2023-07-09 11:48:39 +0300

    done

--- BinaryRepresentation/BitsManipulation.cs
+++ BinaryRepresentation/BitsManipulation.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace BinaryRepresentation
 {
@@ -11,7 +12,25 @@ namespace BinaryRepresentation
         /// <returns>Binary memory representation of signed long integer.</returns>
         public static string GetMemoryDumpOf(long number)
         {
-            throw new NotImplementedException("You need to implement this method.");
+            const int bitsPerByte = 8;
+            const int longSizeInBytes = sizeof(long);
+            const int longSizeInBits = longSizeInBytes * bitsPerByte;
+
+            StringBuilder builder = new StringBuilder(longSizeInBits);
+            for (int i = longSizeInBits - 1; i >= 0; i--)
+            {
+                long mask = 1L << i;
+                long bit = (number & mask) >> i;
+
+                builder.Append(bit);
+            }
+
+            if (number < 0)
+            {
+                builder.Remove(0, 1);
+            }
+
+            return builder.ToString();
         }
     }
 }
